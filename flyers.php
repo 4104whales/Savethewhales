077128d11d5fb3eb060ceaf05d30e7f255b84b6f
bellzax <bellzax@gmail.com>
2016-03-18 00:52:03 -0400

    refactored sing, updated flyers, began updating petitions, ignore dwsync

--- flyers.php
+++ flyers.php
@@ -25,39 +25,66 @@
           </section>
       </div>
       
-      <div class="row">
-          <section class="col-md-4 staff-img">
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
               <img src="images/Carousel_Placeholder.png" alt="">
           </section>
-          <section class="col-md-6 staff-info">
+          <section class="col-md-6">
+              <h3>
+              	Flyer Title
+              </h3>
+              <button type="button" class="pure-button pure-button-primary">Download</button>
+              
+              <h4>
+              	Description
+              </h4>
+              <button type="button" class="pure-button pure-button-primary">Share</button>
               <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra.
-                    <br>
-                    <br>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
               </p>
           </section>
       </div>
       
-      <div class="row">
-          <section class="col-md-offset-2 col-md-2 sing-audio">
-               <audio controls>
-                  <source src="audio/test.ogg" type="audio/ogg">
-                  <source src="audio/test.mp3" type="audio/mpeg">
-                  Your browser does not support the audio element.
-               </audio> 
-                <button type="button" class="pure-button pure-button-primary">Download Sheet Music PDF</button>
-                <button type="button" class="pure-button pure-button-primary">Download Song</button>
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
+              <img src="images/Carousel_Placeholder.png" alt="">
           </section>
-          <section class="col-md-6 staff-info">
+          <section class="col-md-6">
+              <h3>
+              	Flyer Title
+              </h3>
+              <button type="button" class="pure-button pure-button-primary">Download</button>
+              
               <h4>
-              	Lyrics:
+              	Description
               </h4>
+              <button type="button" class="pure-button pure-button-primary">Share</button>
               <p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
               </p>
           </section>
       </div>
+      
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-md-6">
+              <h3>
+              	Flyer Title
+              </h3>
+              <button type="button" class="pure-button pure-button-primary">Download</button>
+              
+              <h4>
+              	Description
+              </h4>
+              <button type="button" class="pure-button pure-button-primary">Share</button>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+              </p>
+          </section>
+      </div>
+      
 
   </div>
   
--- petitions.php
+++ petitions.php
@@ -6,44 +6,48 @@
 
 <body>
 
-<div class="container-fluid">
-	<?php
-		include "includes/header.php";
-    ?>
-  
-  <div class="row">
-    <section class="col-md-12">
-          <h2>
-              Subscribe to newletter
-          </h2>
-          <div>
-          </div>
-      </section>
-  </div>
-  <div class="row">
-    <section class="col-md-4 new_stories">
-        <h2>
-            Learn
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Teach
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Volunteer
-        </h2>
-    </section>
-  </div>
-  <div class="row">
-    <section class="col-md-12 new_stories">
-        <h2>
-            Because we care
-        </h2>
-    </section>
-  </div>
+  <div class="container-fluid">
+      <?php
+          include "includes/header.php";
+      ?>
+      
+              <div class="row">
+          <section class="col-md-4 column-center">
+                <h1>
+                     Petitions
+                </h1>
+                <h2>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.  
+                </h2>
+            </section>
+        </div>
+        <div class="row">
+          <section class="col-md-6 new_stories">
+              <h3>
+                  Petition Name
+              </h3>
+              <audio controls>
+                  <source src="audio/test.ogg" type="audio/ogg">
+                  <source src="audio/test.mp3" type="audio/mpeg">
+                  Your browser does not support the audio element.
+               </audio> 
+               <button type="button" class="pure-button pure-button-primary">Download Sheet Music PDF</button>
+               <button type="button" class="pure-button pure-button-primary">Download Song</button>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. 
+              </p>
+          </section>
+          <section class="col-md-6 new_stories">
+              <h2>
+                  Lorem ipsum
+              </h2>
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. 
+              </p>
+          </section>
+        </div>
+
   </div>
   
   <?php

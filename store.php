0d49801867daf73d15c2dcc125de127922ee45cb
bellzax <bellzax@gmail.com>
2016-03-18 02:29:07 -0400

    made store match wireframes

--- store.php
+++ store.php
@@ -7,44 +7,122 @@
 <body>
 
 <div class="container-fluid">
-	<?php
-		include "includes/header.php";
-    ?>
-  
-  <div class="row">
-    <section class="col-md-12">
-          <h2>
-              Subscribe to newletter
-          </h2>
-          <div>
-          </div>
-      </section>
+      <?php
+          include "includes/header.php";
+      ?>
+      
+      <div class="row">
+          <section class="col-md-4 column-center">
+                <h1>
+                     SVW Store
+                </h1>
+                <h3>
+                     Proceeds from sale of merchandise will go to funding our Save the Whales education programs. 
+                </h3>
+        </section>
   </div>
-  <div class="row">
-    <section class="col-md-4 new_stories">
-        <h2>
-            Learn
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Teach
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Volunteer
-        </h2>
-    </section>
-  </div>
-  <div class="row">
-    <section class="col-md-12 new_stories">
-        <h2>
-            Because we care
-        </h2>
-    </section>
-  </div>
-  </div>
+        
+        <div class="row store-items">
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+        </div>
+        
+        <div class="row store-items">
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+        </div>
+        
+  <div class="row store-items">
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+          <section class="col-md-4">
+              <img src="images/Carousel_Placeholder.png" alt="">
+              <h3>
+                 Item Name
+              </h3>
+              <h4>
+                  $24.00
+              </h4>
+          </section>
+        </div>
+        
+        <nav class="col-md-4 column-center">
+            <!-- Add class .pagination-lg for larger blocks or .pagination-sm for smaller blocks-->
+            <ul class="pagination">
+              <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+              <li><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
+            </ul>
+        </nav>
+</div>
   
   <?php
       include "includes/footer.php";

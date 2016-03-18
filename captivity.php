648745f9fdbc1cf7c39a2a32e24dfcc85909700b
bellzax <bellzax@gmail.com>
2016-03-18 02:14:10 -0400

    made captivity look like wireframes

--- captivity.php
+++ captivity.php
@@ -6,44 +6,61 @@
 
 <body>
 
-<div class="container-fluid">
-	<?php
-		include "includes/header.php";
-    ?>
+  <div class="container-fluid">
+      <?php
+          include "includes/header.php";
+      ?>
+      
+      <div class="row">
+          <section class="col-md-4 column-center">
+              <h1>
+                  Marine Animals in Captivity
+              </h1>
+          </section>
+      </div>
+      
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-md-6">
+              <h3>
+              	Heading
+              </h3>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+              </p>
+          </section>
+      </div>
+      
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-md-6">
+              <h3>
+              	Heading
+              </h3>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+              </p>
+          </section>
+      </div>
+      
+      <div class="row flyer-info">
+          <section class="col-md-offset-2 col-md-2">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-md-6">
+              <h3>
+              	Heading
+              </h3>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+              </p>
+          </section>
+      </div>
   
-  <div class="row">
-    <section class="col-md-12">
-          <h2>
-              Subscribe to newletter
-          </h2>
-          <div>
-          </div>
-      </section>
-  </div>
-  <div class="row">
-    <section class="col-md-4 new_stories">
-        <h2>
-            Learn
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Teach
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Volunteer
-        </h2>
-    </section>
-  </div>
-  <div class="row">
-    <section class="col-md-12 new_stories">
-        <h2>
-            Because we care
-        </h2>
-    </section>
-  </div>
   </div>
   
   <?php

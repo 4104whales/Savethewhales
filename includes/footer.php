2233cc46ec318ac3165b03bbedc014f2006a351d
bellzax <bellzax@gmail.com>
2016-03-18 10:57:05 -0400

    footer style updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,22 @@
   <footer class="footer">
       <div class="container">
           <div class="row">
-              <section class="col-xs-4 copyright">
+              <section class="col-md-offset-4 col-md-4 copyright">
+                  <div class="social-icons">
+                  	<a href="http://www.facebook.com/pages/Save-The-Whales/257469333794" target="_blank">
+                      <img src="images/icons/facebook.png" alt="facebook icon">
+                    </a>
+                    <a href="https://twitter.com/SaveTheWhalesCA" target="_blank">
+                      <img src="images/icons/twitter.png" alt="twitter icon">
+                    </a>
+                    <a href="https://www.instagram.com/savethewhalesofficial/" target="_blank">
+                      <img src="images/icons/instagram.png" alt="instagram icon">
+                    </a>
+                  </div>
+              </section>
+          </div>
+          <div class="row">
+              <section class="col-md-offset-4 col-md-4 copyright">
                   <ul class="footer-links">
                       <li>
                           <a href="founder.php">
@@ -19,21 +34,10 @@
                           </a>
                       </li>
                   </ul>
-                  <div class="social-icons">
-                  	<a href="http://www.facebook.com/pages/Save-The-Whales/257469333794" target="_blank">
-                      <img src="images/icons/facebook.png" alt="facebook icon">
-                    </a>
-                    <a href="https://twitter.com/SaveTheWhalesCA" target="_blank">
-                      <img src="images/icons/twitter.png" alt="twitter icon">
-                    </a>
-                    <a href="https://www.instagram.com/savethewhalesofficial/" target="_blank">
-                      <img src="images/icons/instagram.png" alt="instagram icon">
-                    </a>
-                  </div>
-              </section>
+                </section>
           </div>
           <div class="row">
-              <section class="col-xs-4 copyright">
+              <section class="col-md-offset-4 col-md-4 copyright">
                    Copyright © 1994-2016 | Save The Whales 
               </section>
           </div>

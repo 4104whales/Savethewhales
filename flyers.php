c96b92339fae2aac3b408ee6383c3beecd5f2496
bellzax <bellzax@gmail.com>
2016-03-18 00:33:32 -0400

    navbar chnages, flyer and sing updated, audio files uploaded
    - nav bar hamburger now left and mobile menu follows below
    - made flyers to wireframe style
    - made sing to wireframe style
    - upload test audio files

--- flyers.php
+++ flyers.php
@@ -6,44 +6,59 @@
 
 <body>
 
-<div class="container-fluid">
-	<?php
-		include "includes/header.php";
-    ?>
-  
-  <div class="row">
-    <section class="col-md-12">
-          <h2>
-              Subscribe to newletter
-          </h2>
-          <div>
-          </div>
-      </section>
-  </div>
-  <div class="row">
-    <section class="col-md-4 new_stories">
-        <h2>
-            Learn
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Teach
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Volunteer
-        </h2>
-    </section>
-  </div>
-  <div class="row">
-    <section class="col-md-12 new_stories">
-        <h2>
-            Because we care
-        </h2>
-    </section>
-  </div>
+  <div class="container-fluid">
+      <?php
+          include "includes/header.php";
+      ?>
+      
+      <div class="row">
+          <section class="col-md-4 column-center">
+              <h1>
+                  Informational Flyers
+              </h1>
+              <h2>
+                  Below are recent and past flyers that Save the Whales has made and handed out to the public. We believe educating others on how to preserve marine life and how it will help preserve the future of the marine animals.
+                  <br>
+                  <br>
+				  Feel free to spread the word in your neighborhood, on the web and at school. Inform others on how to save these majestic Whales and other marine animals. 
+              </h2>
+          </section>
+      </div>
+      
+      <div class="row">
+          <section class="col-md-4 staff-img">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-md-6 staff-info">
+              <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra.
+                    <br>
+                    <br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. 
+              </p>
+          </section>
+      </div>
+      
+      <div class="row">
+          <section class="col-md-offset-2 col-md-2 sing-audio">
+               <audio controls>
+                  <source src="audio/test.ogg" type="audio/ogg">
+                  <source src="audio/test.mp3" type="audio/mpeg">
+                  Your browser does not support the audio element.
+               </audio> 
+                <button type="button" class="pure-button pure-button-primary">Download Sheet Music PDF</button>
+                <button type="button" class="pure-button pure-button-primary">Download Song</button>
+          </section>
+          <section class="col-md-6 staff-info">
+              <h4>
+              	Lyrics:
+              </h4>
+              <p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet.
+              </p>
+          </section>
+      </div>
+
   </div>
   
   <?php

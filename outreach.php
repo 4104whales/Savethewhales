e5b27f529d8edc52ee8ba716e68c57a50d61bac8
bellzax <bellzax@gmail.com>
2016-03-17 23:46:35 -0400

    Revert "updating outreach style to match new wireframe"
    This reverts commit 9f6fab9b8802a6b2463e57c9138d35d338167c3f.

--- outreach.php
+++ outreach.php
@@ -12,7 +12,7 @@
         ?>
         
         <div class="row">
-          <section class="col-md-4 column-center">
+          <section class="col-xs-4 column-center">
               <h1>
                   Whales On Wheels (WOW)
               </h1>
@@ -24,26 +24,11 @@
               </h2>
           </section>
         </div>
-        
         <div class="row">
-          <section class="col-md-offset-2 col-md-6 staff-info">
-              <h2 class="staff-name">
-                  Heading
-              </h2>
-              <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. 
-              </p>
-          </section>
-          <section class="col-md-2 staff-img">
+          <section class="col-xs-4 staff-img">
               <img src="images/Carousel_Placeholder.png" alt="">
           </section>
-        </div>
-        
-        <div class="row">
-          <section class="col-md-4 staff-img">
-              <img src="images/Carousel_Placeholder.png" alt="">
-          </section>
-          <section class="col-md-6 staff-info">
+          <section class="col-xs-8 staff-info">
               <h2 class="staff-name">
                   Heading
               </h2>
@@ -52,9 +37,11 @@
               </p>
           </section>
         </div>
-        
         <div class="row">
-          <section class="col-md-offset-2 col-md-6  staff-info">
+          <section class="col-xs-4 staff-img">
+              <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-xs-8 staff-info">
               <h2 class="staff-name">
                   Heading
               </h2>
@@ -62,16 +49,25 @@
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. 
               </p>
           </section>
-          <section class="col-md-2 staff-img">
+        </div>
+        <div class="row">
+          <section class="col-xs-4 staff-img">
               <img src="images/Carousel_Placeholder.png" alt="">
+          </section>
+          <section class="col-xs-8 staff-info">
+              <h2 class="staff-name">
+                  Heading
+              </h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. 
+              </p>
           </section>
         </div>
-        
         <div class="row">
-          <section class="col-md-4 staff-img">
+          <section class="col-xs-4 staff-img">
               <img src="images/Carousel_Placeholder.png" alt="">
           </section>
-          <section class="col-md-6 staff-info">
+          <section class="col-xs-8 staff-info">
               <h2 class="staff-name">
                   Heading
               </h2>

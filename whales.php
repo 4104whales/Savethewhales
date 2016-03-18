527225d0ec8505c47229ad4ef1169162939a8bc5
bellzax <bellzax@gmail.com>
2016-03-18 02:12:28 -0400

    made whales have accordings and match wireframes, made endangered match wireframes

--- whales.php
+++ whales.php
@@ -7,44 +7,94 @@
 <body>
 
 <div class="container-fluid">
-	<?php
-		include "includes/header.php";
-    ?>
-  
+      <?php
+          include "includes/header.php";
+      ?>
+      
   <div class="row">
-    <section class="col-md-12">
-          <h2>
-              Subscribe to newletter
-          </h2>
-          <div>
+          <section class="col-md-4 column-center">
+              <h1>
+                  Sing to Save the Whales
+              </h1>
+          </section>
+    </div>
+      
+    <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab">
+              <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1">CETACEANS</a></h4>
+            </div>
+            <div id="collapseOne1" class="panel-collapse collapse in">
+              <div class="panel-body">
+              	<h2>
+                	Whales, Dolphins, and Porpises
+                </h2>
+                <section class="col-md-2 staff-img">
+                    <img src="images/Carousel_Placeholder.png" alt="">
+                </section>
+                <section class="col-md-10 flyer-info">
+                    <p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+                  </p>
+                </section>
+              </div>
+            </div>
           </div>
-      </section>
-  </div>
-  <div class="row">
-    <section class="col-md-4 new_stories">
-        <h2>
-            Learn
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Teach
-        </h2>
-    </section>
-    <section class="col-md-4 new_stories">
-        <h2>
-            Volunteer
-        </h2>
-    </section>
-  </div>
-  <div class="row">
-    <section class="col-md-12 new_stories">
-        <h2>
-            Because we care
-        </h2>
-    </section>
-  </div>
-  </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo1">Whale Research</a></h4>
+            </div>
+            <div id="collapseTwo1" class="panel-collapse collapse">
+              <div class="panel-body">
+              	<h2>
+                	Whale Research
+                </h2>
+                <section class="col-md-2 staff-img">
+                    <img src="images/Carousel_Placeholder.png" alt="">
+                </section>
+                <section class="col-md-10 flyer-info">
+                    <p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+                  </p>
+                </section>
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion1" href="#collapseThree1">Whale Watching Guidelines</a></h4>
+            </div>
+            <div id="collapseThree1" class="panel-collapse collapse">
+              <div class="panel-body">
+              	<h2>
+                	Whale Watching Guidelines
+                </h2>
+                <section class="col-md-2 staff-img">
+                    <img src="images/Carousel_Placeholder.png" alt="">
+                </section>
+                <section class="col-md-10 flyer-info">
+                    <p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+                  </p>
+                  <div>
+                      <ol>
+                        <li>
+                            Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.
+                        </li>
+                        <li>
+                            Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.
+                        </li>
+                        <li>
+                            Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.
+                        </li>
+                      </ol>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+    </div>
+</div>
   
   <?php
       include "includes/footer.php";
